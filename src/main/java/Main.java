12d8fa030b2542b20ebfce7ec4fb54a3260836d5
meSalpiglossis <125427233+meSalpiglossis@users.noreply.github.com>
2023-06-01 04:10:31 +1000

    release 1.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,25 +1,49 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
-    public static String calc(String input) throws Exception {
+    public static void main(String[] args) {
+        calc("");
+    }
 
-        //a regular expression that matches the conditions of the task(hopefully)
+    public static String calc(String input) {
+        //a regular expression that matches the conditions of the task (hopefully)
         final String regex = "^(?:[1-9]|10) ?[\\+\\-\\*\\/] ?(?:[1-9]|10)$";
 
+        System.out.println("Input:");
+
+        if (input == "") {
+            input = consoleInput();
+        } else {
+            System.out.println(input);
+        }
+
         if (!input.matches(regex)) {
+            try {
                 throw new Exception("Invalid input");
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
         }
 
         int[] operand = Arrays.stream(input.split(" ?[\\+\\-\\*\\/] ?")).mapToInt(Integer::parseInt).toArray();
 
         String operator = input.replaceAll("[^+\\-*\\/]", "");
 
-        return switch (operator) {
+        String result = switch (operator) {
             case "+" -> String.valueOf(operand[0] + operand[1]);
             case "-" -> String.valueOf(operand[0] - operand[1]);
             case "*" -> String.valueOf(operand[0] * operand[1]);
             case "/" -> String.valueOf(operand[0] / operand[1]);
             default -> "";
         };
+
+        System.out.println("\nOutput:\n" + result);
+
+        return result;
+    }
+
+    private static String consoleInput() {
+        return new Scanner(System.in).nextLine();
     }
 }
